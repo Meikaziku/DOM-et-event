bc762ae74a066e0b3e8f6e83add2924b91272e72
Meikaziku <yanis.marouani13@gmail.com>
2025-10-03 13:58:54 +0200

    exercice 2

---
+++ partie 1/exercice 2/main.js
@@ -0,0 +1,9 @@
+let formulaire = document.querySelector("#lastname")
+
+formulaire.addEventListener("blur", focusBlur);
+function focusBlur(event) {
+
+  alert("Merci de votre participation")
+  
+
+}
