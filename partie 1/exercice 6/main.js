4c15cff15ce38f427a477ddb9f0aca6f21b2cab5
Meikaziku <yanis.marouani13@gmail.com>
2025-10-06 15:40:54 +0200

    exercice 6

--- partie 1/exercice 6/main.js
+++ partie 1/exercice 6/main.js
@@ -1,15 +1,26 @@
+let imgs = document.querySelectorAll("img");
 
-let formulaire = document.querySelector("#formulaire")
+imgs.forEach((img) => {
 
 
+  img.addEventListener("mouseenter", replaceImgOnMouseEnter);
+});
 
-formulaire.addEventListener("reset", formulaireReset)
+function replaceImgOnMouseEnter(event) {
+  let imgTag = event.target;
+  console.log(imgTag);
 
-function formulaireReset(event) {
-
-    
-
-    
-    
+  imgTag.src = imgTag.src.replace(".jpg", "_2.jpg");
+  imgTag.removeEventListener("mouseenter", replaceImgOnMouseEnter);
+  imgTag.addEventListener("mouseleave", replaceImgOnMouseLeave);
+  
 }
 
+function replaceImgOnMouseLeave(event) {
+  let imgTag = event.target;
+  console.log(imgTag);
+
+  imgTag.src = imgTag.src.replace("_2.jpg", ".jpg");
+  imgTag.removeEventListener("mouseleave", replaceImgOnMouseLeave);
+  imgTag.addEventListener("mouseenter", replaceImgOnMouseEnter);
+}
