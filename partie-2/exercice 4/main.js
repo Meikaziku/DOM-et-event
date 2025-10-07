7cc284f111ccd42f9662450432ce730c62961f9c
Meikaziku <yanis.marouani13@gmail.com>
2025-10-07 13:45:28 +0200

    fin

---
+++ partie-2/exercice 4/main.js
@@ -0,0 +1,20 @@
+// let formulaire = document.querySelector("#formulaire")
+let mdpAtm = document.querySelectorAll("input#password")
+let mdpConfirmation = document.querySelector("#password")
+console.log(mdpConfirmation);
+
+
+
+if (PmdpAtm = mdpConfirmation) {
+
+    console.log("heelo");
+    
+}
+
+
+
+
+
+
+
+
